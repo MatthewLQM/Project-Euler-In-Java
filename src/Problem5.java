dfda61215e1f0d0b1a5dfd83da242e26388b7001
MatthewLQM <MatthewLQM@Gmail.com>
2018-01-20 12:07:16 +0800

    add Problem 5

---
+++ src/Problem5.java
@@ -0,0 +1,18 @@
+/**
+ * Project Euler in Java
+ * @Author MatthewLQM
+ *
+ * Problem 5 Smallest multiple
+ * 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+ * What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+ *
+ */
+public class Problem5 {
+    public static void main(String[] argv) {
+        // I solve this question totally by math knowledge without programming;
+        // My idea is that if the number can be divided by both 3 and 5, then this number
+        // can be divided by 15 too, and so on.
+        int result = 3 * 5 * 7 * 3 * 11 * 13 * 16 * 17 * 19;
+        System.out.println("The result is " + result);
+    }
+}
