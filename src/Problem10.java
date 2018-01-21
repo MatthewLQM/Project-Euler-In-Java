8c25bef07444c22894cf38e5564a8b76f5777c89
MatthewLQM <MatthewLQM@Gmail.com>
2018-01-21 14:54:33 +0800

    add problem 10

---
+++ src/Problem10.java
@@ -0,0 +1,17 @@
+/**
+ * Project Euler in Java
+ * @author MatthewLQM
+ *
+ * Problem 10 Summation of primes
+ * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+ * Find the sum of all the primes below two million.
+ */
+public class Problem10 {
+    public static void main(String[] argv) {
+        long result = 2L;
+        for(long i = 3L; i < 2000000; i += 2) {
+            if(Tools.isPrime(i)) result += i;
+        }
+        System.out.println("The result is " + result);
+    }
+}
