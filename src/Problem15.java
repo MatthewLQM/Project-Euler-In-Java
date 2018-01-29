0cd94aa519ba4b5a99797a1e589c74e8e3e7117d
MatthewLQM <MatthewLQM@Gmail.com>
2018-01-29 16:59:19 +0800

    add problem 15

---
+++ src/Problem15.java
@@ -0,0 +1,21 @@
+/**
+ * Project Euler in Java
+ *
+ * @author MatthewLQM
+ *
+ * Problem 15 Lattice paths
+ * Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down,
+ * there are exactly 6 routes to the bottom right corner.
+ * How many such routes are there through a 20×20 grid?
+ */
+public class Problem15 {
+
+    public static void main(String[] argv) {
+        long result = 1;
+        for(int i = 0; i < 20; i++) {
+            result *= 40 - i;
+            result /= i + 1;
+        }
+        System.out.println("The result is " + result);
+    }
+}
