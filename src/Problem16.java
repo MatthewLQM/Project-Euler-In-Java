93cb5f8464b44cac2461085c4e4505a0cb06f58e
MatthewLQM <MatthewLQM@Gmail.com>
2018-01-30 11:02:22 +0800

    add problem 16

---
+++ src/Problem16.java
@@ -0,0 +1,25 @@
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+/**
+ * Project Euler in Java
+ *
+ * @author MatthewLQM
+ *
+ * Problem 16 Power digit sum
+ * 215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+ * What is the sum of the digits of the number 21000?
+ */
+public class Problem16 implements Problem {
+    @Override
+    public int solve() {
+        int result = 0;
+        BigInteger number = BigInteger.valueOf(2);
+        number = number.pow(1000);
+        while (number.compareTo(BigInteger.ZERO) == 1) {
+            result += number.mod(BigInteger.TEN).intValue();
+            number = number.divide(BigInteger.TEN);
+        }
+        return result;
+    }
+}
